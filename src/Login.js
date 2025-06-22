6fbb7b7329123e5a1bc0718cd46bbac2319a04ad
luizaki <francinelouise.sanchez@gmail.com>
2025-06-23 00:24:44 +0800

    Add login page

---
+++ src/Login.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className="login-container">
+            <form className="login-form">
+            <h2 className="login-title">Login Page</h2>
+
+            <div className="input-field">
+                <input type="text" id="username" name="username" required
+                value={username} onChange={(e) => setUsername(e.target.value)} />
+                <label for="username">Username</label>
+            </div>
+
+            <div className="input-field">
+                <input type="password" id="password" name="password" required
+                value={password} onChange={(e) => setPassword(e.target.value)} />            
+                <label for="password">Password</label>
+            </div>
+
+            <button type="submit" className="login-button">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
